7978e996c1fe5e2f2657d67f715923ce43e26d99
Aditya Mandlekar <aditya.mandlekar@thomsonreuters.com>
2019-12-28 07:41:16 -0500

    SpringBoot MS Implementation - Customer

---
+++ Customer/Customer/src/test/java/com/SpringbootMS/Customer/Customer/CustomerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.SpringbootMS.Customer.Customer;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
+@SpringBootTest
+class CustomerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
