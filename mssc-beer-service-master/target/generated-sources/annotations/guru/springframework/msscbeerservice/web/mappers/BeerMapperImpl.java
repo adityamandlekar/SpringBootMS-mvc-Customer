1affd5f1f4a1670888e4124830d52d00c73a43ef
Aditya Mandlekar <aditya.mandlekar@thomsonreuters.com>
2019-12-29 10:10:53 -0500

    Live Project Spring Framework Course

---
+++ mssc-beer-service-master/target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -0,0 +1,15 @@
+package guru.springframework.msscbeerservice.web.mappers;
+
+import javax.annotation.processing.Generated;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2019-12-29T10:07:57-0500",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.5 (Oracle Corporation)"
+)
+@Component
+@Primary
+public class BeerMapperImpl extends BeerMapperDecorator implements BeerMapper {
+}
